b48ff5e0035166e2234cdd8490ad94385f058a9f
zhujinrun <15997352948@163.com>
2019-11-13 12:52:50 +0800

    add env  configure file

--- admin/src/views/About.vue
+++ admin/src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <h1>base-api: {{url}}</h1>
+    </div>
 </template>
+
+<script>export default {
+  data () {
+    return {
+      url: process.env.VUE_APP_BASE_URL + ':' + process.env.VUE_APP_BASE_PORT
+    }
+  }
+}</script>
